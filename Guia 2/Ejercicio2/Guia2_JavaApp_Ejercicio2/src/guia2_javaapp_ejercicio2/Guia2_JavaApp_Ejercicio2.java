608f7f77cd8bfcc07e2581ea26f039e9c2ba619c
Mettal94 <alan.j.schembri@gmail.com>
2023-08-13 17:27:13 -0300

    Ejercicio 2 re hecho

---
+++ Guia 2/Ejercicio2/Guia2_JavaApp_Ejercicio2/src/guia2_javaapp_ejercicio2/Guia2_JavaApp_Ejercicio2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia2_javaapp_ejercicio2;
+
+/**
+ *
+ * @author PC
+ */
+public class Guia2_JavaApp_Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
